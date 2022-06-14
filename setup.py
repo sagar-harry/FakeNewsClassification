e8d19c09db825b1c71389598576a1b0e646f94d9
Konni Vidya Sagar <RH0957@accionlabs.in>
2022-06-14 15:52:27 +0530

    Setup.py added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="Fake news classification model",
+    author="Konni Vidya Sagar",
+    packages=find_packages()
+)
