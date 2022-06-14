602fe50f13ffba2d93e439a3858dfac12b97bef0
Konni Vidya Sagar <RH0957@accionlabs.in>
2022-06-14 22:27:30 +0530

    app.py created and tested but isn't automated

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, request, jsonify
+import os
+import yaml
+import joblib
+
+params_path = "params.yaml"
+webapp_root = "webapp"
+
+
+static_dir = os.path.join(webapp_root, "static")
+template_dir = os.path.join(webapp_root, "templates")
+app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
+
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+def predict(title, content):
+    config = read_params(params_path)
+    model_dir_path = config["model-dir"]
+    model = joblib.load(os.path.join(model_dir_path, "model-1.joblib"))
+    prediction = model.predict([title+"---"+content])
+    return prediction[0]
+
+def api_response(request):
+    try:
+        title, content = request.json.values()
+        response = "FAKE NEWS"
+        if predict(title, content)==1:
+            response="REAL NEWS"
+        response_ = {"response": response}
+        return response_
+    except:
+        print("Error has occured")
+        return {"error":"Something went wrong"}
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        try:
+            if request.form:
+                title = request.form["title"]
+                content = request.form["content"]
+                response_ = predict(title, content)
+                news_truth = "REAL NEWS"
+                if response_[0]==0:
+                    news_truth="FAKE NEWS"
+                return render_template("index.html", response_=news_truth)
+            elif request.json:
+                response_ = api_response(request)
+                print(response_)
+                print(jsonify(response_))
+                return jsonify(response_)
+        except:
+            print("Error occured")
+    else:
+        return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
