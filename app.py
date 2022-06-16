309873778cf993a6ee5f9c4aeb015028ebb469f6
Konni Vidya Sagar <RH0957@accionlabs.in>
2022-06-16 07:32:19 +0530

    404-page added, improved test cases!

--- app.py
+++ app.py
@@ -1,39 +1,13 @@
 from flask import Flask, render_template, request, jsonify
 import os
-import yaml
-import joblib
+from prediction_service import prediction
 
-params_path = "params.yaml"
 webapp_root = "webapp"
 
 
 static_dir = os.path.join(webapp_root, "static")
 template_dir = os.path.join(webapp_root, "templates")
 app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
-
-def read_params(config_path):
-    with open(config_path) as yaml_file:
-        config = yaml.safe_load(yaml_file)
-    return config
-
-def predict(title, content):
-    config = read_params(params_path)
-    model_dir_path = config["prediction"]["final-model"]
-    model = joblib.load(model_dir_path)
-    prediction = model.predict([title+"---"+content])
-    return prediction[0]
-
-def api_response(request):
-    try:
-        title, content = request.json.values()
-        response = "FAKE NEWS"
-        if predict(title, content)==1:
-            response="REAL NEWS"
-        response_ = {"response": response}
-        return response_
-    except:
-        print("Error has occured")
-        return {"error":"Something went wrong"}
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -43,19 +17,22 @@ def index():
             if request.form:
                 title = request.form["title"]
                 content = request.form["content"]
-                response_ = predict(title, content)
+                response_ = prediction.predict(title, content)
                 news_truth = "REAL NEWS"
                 if response_==0:
                     news_truth="FAKE NEWS"
+                elif response_==-1:
+                    news_truth="Enter a valid value"
                 return render_template("index.html", response_=news_truth)
             elif request.json:
-                response_ = api_response(request)
+                response_ = prediction.api_response(request)
                 return jsonify(response_)
-        except:
-            print("Error occured")
+        except Exception as e:
+            return render_template("404.html", error=e)
     else:
         return render_template("index.html")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
---
+++ prediction_service/prediction.py
@@ -0,0 +1,59 @@
+import yaml
+import joblib
+import numpy as np
+import os
+import json
+
+params_path = "params.yaml"
+webapp_root = "webapp"
+
+
+class NotAString(Exception):
+    def __init__(self, message="Either or both title and text are not a string"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class NotAValidValue(Exception):
+    def __init__(self, message="Text is empty"):
+        self.message = message
+        super().__init__(self.message)
+
+
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+
+def predict(title, content):
+    if validate_input(title=title, content=content):
+        config = read_params(params_path)
+        model_dir_path = config["prediction"]["final-model"]
+        model = joblib.load(model_dir_path)
+        prediction = model.predict([title+"---"+content])
+        return prediction[0]
+    else: return -1
+
+
+def validate_input(title, content):
+    if (title.isnumeric() or content.isnumeric()):
+        raise NotAString
+    elif (len(content) == 0):
+        raise NotAValidValue
+    else: return True
+
+
+def api_response(request):
+    try:
+        title, content = request.json.values()
+        response = "FAKE NEWS"
+        model_output = predict(title, content)
+        if model_output == 1:
+            response = "REAL NEWS"
+        elif model_output == -1:
+            response = "Not a valid input"
+        response_ = {"response": response}
+        return response_
+    except Exception as e:
+        return {"error": e}
